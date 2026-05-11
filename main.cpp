fde78bd3643ab6f2234b9beb90072e9699271135
Zacncheeez <zmt.tobin@gmail.com>
2026-05-10 21:00:27 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,44 +1,63 @@
 #include <iostream>
+#include <stack>
+#include <map>
+#include <string>
 
+struct TreeNode {
+    std::string value;
+    TreeNode* left;
+    TreeNode* right;
 
-//Start code with what I need for later
+    TreeNode(std::string v) : value(v), left(nullptr), right(nullptr) {}
+};
 
-
-int main()
+int main() 
 {
-    
-    precedence = {
-    '^': 3,
-    '*': 2,
-    '/': 2,
-    '+': 1,
-    '-': 1
+    // operator precedence
+    std::map<char, int> precedence = {
+        {'^', 3},
+        {'*', 2},
+        {'/', 2},
+        {'+', 1},
+        {'-', 1}
+    };
+
+    // right associativity
+    std::map<char, bool> right_assoc = {
+        {'^', true},
+        {'*', false},
+        {'/', false},
+        {'+', false},
+        {'-', false}
+    };
+
+    // placeholder: your postfix expression goes here
+    std::string postfix = "";  
+
+    std::stack<TreeNode*> stack;
+
+    // loop through postfix tokens
+    for (char token : postfix)
+    {
+        if (isdigit(token)) {
+            TreeNode* node = new TreeNode(std::string(1, token));
+            stack.push(node);
+        }
+        else { // operator
+            TreeNode* right = stack.top(); stack.pop();
+            TreeNode* left  = stack.top(); stack.pop();
+
+            TreeNode* node = new TreeNode(std::string(1, token));
+            node->left = left;
+            node->right = right;
+
+            stack.push(node);
+        }
+    }
+
+    TreeNode* root = nullptr;
+    if (!stack.empty())
+        root = stack.top();
+
+    return 0;
 }
-
-# True = right associative (only ^)
-right_assoc = {
-    '^': True,
-    '*': False,
-    '/': False,
-    '+': False,
-    '-': False
-}
-
-
-    for token in postfix:
-    if token is a number:
-        node = new TreeNode(token)
-        stack.push(node)
-
-    else if token is an operator:
-        right = stack.pop()
-        left = stack.pop()
-        node = new TreeNode(token)
-        node.left = left
-        node.right = right
-        stack.push(node)
-
-root = stack.pop()
-
-}
-
